243a469fbc648df7be0d4007ec3c2315f8131732
David Lucca <david.v.lucca@gmail.com>
2024-05-07 13:34:23 +0100

    Working on tailwindCSS for the Hero component

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -7,15 +7,15 @@ function Hero() {
   const { t } = useTranslation("global");
 
   return (
-    <div className="px-4 mM:px-2 mS:px-0">
+    <div className="md:px-6 px-4 mM:px-2 mS:px-0">
       <div className="hero py-12 mM:py-8 mS:py-4  ">
         <div className="hero-content flex-col xl:flex-row xl:space-x-10">
           <img src="/me.jpg" className=" rounded-full shadow-2xl" />
-          <div className="text-left pt-10 m-0 p-0">
-            <h3 className="text-white text-xl font-bold px-4 py-2 rounded-xl w-fit bg-[#333333]/50">
+          <div className="text-left px-4 mS:px-0 pt-10 m-0 p-0">
+            <h3 className="text-white mS:text-base mM:text-lg md:text-2xl text-xl font-bold px-3 py-2 rounded-xl w-fit bg-[#333333]/50">
               🧐 About me
             </h3>
-            <div className="py-6 text-lg space-y-3">
+            <div className="py-6 md:text-xl text-lg space-y-3">
               <p>{t("hero.desc1")}</p>
               <p>{t("hero.desc2")}</p>
               <p>{t("hero.desc3")}</p>
